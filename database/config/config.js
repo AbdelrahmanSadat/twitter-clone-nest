7dd88015b60709b0815ef6d6b6b136f5b24abb68
Abdelrahman Sadat <abdelrahmansadat1100001@gmail.com>
2021-08-12 00:35:39 +0200

    - fix?: continuation on last issue - straight up writing credentials into config file - this is very very very bad

--- database/config/config.js
+++ database/config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     dialect: process.env.DB_DIALECT
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_PORT,
+    username: "sinrxanwetrdpy",
+    password: "8b0a6c6c91c64e2fa06d6bc6ba22809d4e2890bf6f0089b42df6a43faec54d00",
+    database: "d3egonipj2lf12",
+    host: "ec2-52-208-221-89.eu-west-1.compute.amazonaws.com",
     dialect: "postgres",
     "operatorsAliases": false,
     "use_env_variable": "DATABASE_URL"
